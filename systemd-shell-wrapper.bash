f10c82cabc58ee04c85494bfa79de7e85cc1334d
Denis A. Altoé Falqueto <denisfalqueto@gmail.com>
2012-10-24 23:01:26 -0200

    Make the scripts independent of unit type
    Signed-off-by: Denis A. Altoé Falqueto <denisfalqueto@gmail.com>

--- systemd-shell-wrapper.bash
+++ systemd-shell-wrapper.bash
@@ -23,13 +23,25 @@ s.list()        { s_list_services "list"; }
 s.log()         { s_journalctl "$@"; }
 s.tree()        { s_exec "systemd-cgls --all"; }
 
+# $1: unit name
+# $2: optional suffix. It must not have a preceeding dot. Default one is .service
+s_append_type() {
+	defType=".${2:-service}"
+	if [[ ! $1 =~ \. ]]; then
+		echo "$1$defType"
+	else
+		echo "$1"
+	fi
+}
+
 s_systemctl() {
-	daemon="${2/.service/}"
-	if [[ "$daemon" == "" || ! $(s_daemon_exists $daemon) ]]; then echo -e "\e[1;31m:: \e[1;37m $daemon daemon does not exist\e[0m"; return; fi
+	unitType=${3:-service}
+	daemon="$(s_append_type $2 $unitType)"
+	if [[ "$daemon" == "" || $(s_daemon_exists $daemon $unitType) ]]; then echo -e "\e[1;31m:: \e[1;37m $daemon daemon does not exist\e[0m"; return; fi
 	s_exec "/bin/true" # if sudo then ask for password now to avoid messing up the output later
 	case $1 in
 		start|stop|restart|reload)
-			${_systemctl} -q is-active "${daemon}"".service" >& /dev/null
+			systemctl -q is-active "${daemon}" >& /dev/null
 			if [[ $? -eq 0 ]]; then
 				if [[ "$1" == "start" ]]; then echo -e "\e[1;31m:: \e[1;37m $daemon daemon is already running\e[0m"; return; fi
 			else
@@ -42,12 +54,12 @@ s_systemctl() {
 			if [[ "$1" == "stop" ]];    then echo -en "\e[1;34m:: \e[1;37m Stopping $daemon daemon\e[0m"; cols=25; fi
 			if [[ "$1" == "restart" ]]; then echo -en "\e[1;34m:: \e[1;37m Restarting $daemon daemon\e[0m"; cols=27; fi
 			if [[ "$1" == "reload" ]];  then echo -en "\e[1;34m:: \e[1;37m Reloading $daemon daemon\e[0m"; cols=26; fi
-			s_exec "${_systemctl} -q ""${1}"" ""${daemon}"".service"
+			s_exec "${_systemctl} -q ${1} ${daemon}"
 			if [[ $? -eq 0 ]]; then s_msg $daemon $cols 7 "DONE"; else s_msg $daemon $cols 1 "FAIL"; s_systemctl "status" $daemon; fi
 			;;
 		enable|disable)
 			if [[ "${daemon%%@*}" == "${daemon}" ]]; then # sadly is-enabled does not work as expected for "@" services like dhcpcd@eth0
-				${_systemctl} -q is-enabled "${daemon}"".service" >& /dev/null
+				${_systemctl} -q is-enabled "${daemon}" >& /dev/null
 				if [[ $? -eq 0 ]]; then
 					if [[ "$1" == "enable" ]]; then echo -e "\e[1;31m:: \e[1;37m $daemon daemon is already enabled\e[0m"; return; fi
 				else
@@ -57,22 +69,22 @@ s_systemctl() {
 			f=${1:0:1}
 			echo -en "\e[1;34m:: \e[1;37m ""${f^^}""${1:1:${#1}-2}""ing $daemon daemon\e[0m"
 			if [[ "$1" == "enable" ]]; then cols=25; else cols=26; fi
-			s_exec "${_systemctl} -q ""${1}"" ""${daemon}"".service"
+			s_exec "${_systemctl} -q ${1} ${daemon}"
 			if [[ $? -eq 0 ]]; then s_msg $daemon $cols 7 "DONE"; else s_msg $daemon $cols 1 "FAIL"; fi
 			;;
 		status)	
-			s_exec "${_systemctl}"" status ""${daemon}"".service"
+			${_systemctl} status ${daemon}
 			;;
 	esac
 }
 
 s_journalctl() {
-	daemon=${@:$#}
-	if s_daemon_exists "${daemon/.service/}"; then
+	daemon="$(s_append_type ${@:$#})"
+	if s_daemon_exists "${daemon}"; then
 		options=""; for ((i=1; i<$#; ++i )) ; do options="${options}""${!i}"" "; done
-		s_exec "${_journalctl}"" --all ""$options"" _SYSTEMD_UNIT=""${daemon/.service/}"".service";
+		s_exec "${_journalctl} --all $options _SYSTEMD_UNIT=${daemon}";
 	else
-		s_exec "${_journalctl}"" --all ""$*";
+		s_exec "${_journalctl} --all $*";
 	fi
 }
 
@@ -84,7 +96,7 @@ s_list_services () { $_systemctl --no-legend -t service list-unit-files | grep -
 				if [[ -h "/usr/lib/systemd/system/$service" ]]; then continue; fi
 				
 				# support for "@" stuff like dhcpcd@eth0 dhcpcd@eth1 ...
-				daemon="${service/.service/}"
+				daemon="$(s_append_type ${service})"
 				if [[ "${daemon:${#daemon}-1}" == "@" ]]; then
 					if s_hidedaemon ${daemon}; then continue; fi;
 					daemons=$(${_systemctl} --no-legend -t service | grep -o "${daemon}[A-Za-z0-9_/=:-]*")
@@ -101,7 +113,7 @@ s_list_services () { $_systemctl --no-legend -t service list-unit-files | grep -
 						if [[ "${1}" == "${daemonstate}" ]]; then printf "%s\n" "${daemon}"; fi
 						continue
 					fi
-					${_systemctl} -q is-active "${daemon}"".service" >& /dev/null
+					${_systemctl} -q is-active "${daemon}" >& /dev/null
 					if [[ $? -eq 0 ]]; then
 							if [[ "${1}" == "list" ]]; then
 								echo -en "\e[1;37mSTARTED"
@@ -132,8 +144,10 @@ s_list_services () { $_systemctl --no-legend -t service list-unit-files | grep -
 		}
 }
 
+# $1: Optional type of unit. The default is service
 s_daemon_exists() {
-	if $_systemctl --no-legend -t service list-unit-files | grep -v static | grep -q "^${1%%@*}@*\.service" >& /dev/null; then return 0; else return 1; fi
+	unitType="${2:-service}"
+	if ${_systemctl} --no-legend -t "$unitType" list-unit-files | grep -v static | grep -q "^${1%%@*}@*\.$unitType" >& /dev/null; then return 0; else return 1; fi
 }
 
 s_msg() {
@@ -141,22 +155,25 @@ s_msg() {
 }
 
 s_exec() {
-	if [[ $EUID -ne 0 ]]; then sudo $1; else $1; fi
+	if [[ $EUID -ne 0 ]]; then eval "sudo $@"; else eval "$@"; fi
 }
 
 s_hidedaemon() {
 	for hidedaemon in ${HIDEDAEMONS[@]}; do if [[ "$1" == "$hidedaemon" ]]; then return 0; fi; done; return 1;
 }
 
-s_bashcompletion_list_targets () { $_systemctl --no-legend -t target list-unit-files  \
-	|	{ while read -r a b  ; do printf "%s\n" "${a}"; done; }
+# $1: optional type
+s_bashcompletion_list_by_type () { 
+	unitType=${1:-service}
+	${_systemctl} --no-legend -t $unitType list-unit-files  \
+		|	{ while read -r a b  ; do printf "%s\n" "${a}"; done; }
 }
 
 s_bashcompletion () {
 	local cur=${COMP_WORDS[COMP_CWORD]} prev=${COMP_WORDS[COMP_CWORD-1]}
    local verb comps
 	
-	if [[ "${1}" == "targets" ]]; then comps=$( s_bashcompletion_list_targets );	else comps=$( s_list_services "${1}" ); fi
+	if [[ "${1}" == "targets" ]]; then comps=$( s_bashcompletion_list_by_type "target" );	else comps=$( s_list_services "${1}" ); fi
 	COMPREPLY=( $(compgen -W '$comps' -- "$cur") )
 }
 
@@ -178,4 +195,4 @@ complete -F s_bashcompletion_enable s.enable
 complete -F s_bashcompletion_disable s.disable
 complete -F s_bashcompletion_status s.status
 complete -F s_bashcompletion_wants s.wants
-complete -F s_bashcompletion_log s.log
+complete -F s_bashcompletion_log s.log#
